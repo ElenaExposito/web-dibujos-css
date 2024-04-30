d81ba8f8907bb1e27ef6f1a2dbea57fc22afb239
Elena Expósito <elexla97@gmail.com>
2024-04-30 11:29:37 +0200

    Change Header and consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dibujos con CSS";
+export const SITE_DESCRIPTION =
+  "Bienvenido a mi página sobre dibujos hechos con CSS";
